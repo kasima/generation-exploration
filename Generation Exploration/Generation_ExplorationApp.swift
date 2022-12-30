193f039262639426b00672806ff897d628e2ec22
Kasima Tharnpipitchai <kasima@gmail.com>
2022-12-30 23:50:01 +0700

    Initial Commit

---
+++ Generation Exploration/Generation_ExplorationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Generation_ExplorationApp.swift
+//  Generation Exploration
+//
+//  Created by kasima on 12/30/22.
+//
+
+import SwiftUI
+
+@main
+struct Generation_ExplorationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
